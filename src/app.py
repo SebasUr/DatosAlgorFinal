662eceb451353e7eccc9a3f5763d0affbd6c231b
Tazana2 <danisanmez@gmail.com>
2023-11-19 15:43:20 -0500

    The structure is changed and the BackEnd is finished

---
+++ src/app.py
@@ -0,0 +1,75 @@
+from modules.Graph import Graph
+from modules.Node import Node
+from modules.Chat import Chat
+import random, os
+from flask import Flask, redirect
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return redirect("/leaderboard")
+
+@app.route("/leaderboard")
+def main_app():
+    graph_users = Graph()
+    num_users = random.randint(5, 15) # Número random de usuarios entre 5 y 15
+    names = ["Ana", "Juan", "María", "Carlos", "Laura", "Pedro", "Sofía", "Miguel", "Isabel", "Diego", "Elena", "Javier", "Carmen", "Francisco", "Luis", "Patricia", "Antonio", "Rosa", "José", "Lucía", "Raúl", "Silvia", "Manuel", "Victoria", "Fernando", "Eva", "Alberto", "Natalia", "Roberto", "Marta"]
+    
+    used_names = []
+    # Se ingresan la cantidad de usuarios randoms al grafo
+    for _ in range(num_users):
+        name = random.choice(names)
+
+        while name in used_names:
+            name = random.choice(names)
+        
+        graph_users.add_node(Node(name))
+        used_names.append(name)
+
+
+    # Se agregan las relaciones entre los usuarios existentes en el grafo
+    visited_edges_set = set()
+    nodes_list = list(graph_users.nodes.keys())
+    num_nodes = len(nodes_list)
+    for i in range(1, 25):
+        # Intentamos seleccionar nodos diferentes al menos 5 veces
+        for _ in range(5):
+            node_a = random.choice(nodes_list)
+            node_b = random.choice(nodes_list)
+
+            # Verificamos que los nodos no sean iguales y que la relación no exista
+            if node_a != node_b and frozenset([node_a, node_b]) not in visited_edges_set:
+                break
+        else:
+            # Si el bucle for no se rompe (no encontramos nodos válidos), continuamos con la siguiente iteración
+            continue
+
+        # Crear el chat entre los dos usuarios
+        filename = f"chat{i}.txt"
+        filepath = os.path.join("src", "chats", filename)
+
+        new_chat = Chat(node_a, node_b)
+        new_chat.add_lines(filepath)
+
+        # Hacer que la arista que conecta a los dos usuarios/nodos sea el chat
+        graph_users.add_edge(node_a, node_b, new_chat)
+
+        # Agregar la relación a la lista de relaciones ya existentes
+        visited_edges_set.add(frozenset([node_a, node_b]))
+
+    users_result = graph_users.get_nodes_with_most_edges()
+    result = {
+        'popular_users': [users_result[0], users_result[2]],
+        'top_3_users': enumerate(users_result[1]),
+        'number_edges_top_3': users_result[3],
+        'stronger_relationships': [relationship.weight for relationship in graph_users.get_strongest_edge()]
+    }
+
+    return render_template("index.html", result=result)
+    # return render_template("index.html")
+
+if(__name__=="__main__"): 
+    app.run(debug=True) 
---
+++ src/console.py
@@ -0,0 +1,70 @@
+from modules.Graph import Graph
+from modules.Node import Node
+from modules.Chat import Chat
+import random
+
+if __name__ == "__main__":  
+    graph_users = Graph()
+    num_users = random.randint(5, 15) # Número random de usuarios entre 5 y 15
+    names = ["Ana", "Juan", "María", "Carlos", "Laura", "Pedro", "Sofía", "Miguel", "Isabel", "Diego", "Elena", "Javier", "Carmen", "Francisco", "Luis", "Patricia", "Antonio", "Rosa", "José", "Lucía", "Raúl", "Silvia", "Manuel", "Victoria", "Fernando", "Eva", "Alberto", "Natalia", "Roberto", "Marta"]
+    
+    used_names = []
+    # Se ingresan la cantidad de usuarios randoms al grafo
+    for _ in range(num_users):
+        name = random.choice(names)
+
+        while name in used_names:
+            name = random.choice(names)
+        
+        graph_users.add_node(Node(name))
+        used_names.append(name)
+
+
+    # Se agregan las relaciones entre los usuarios existentes en el grafo
+    visited_edges_set = set()
+    nodes_list = list(graph_users.nodes.keys())
+    num_nodes = len(nodes_list)
+    for i in range(1, 25):
+        # Intentamos seleccionar nodos diferentes al menos 5 veces
+        for _ in range(5):
+            node_a = random.choice(nodes_list)
+            node_b = random.choice(nodes_list)
+
+            # Verificamos que los nodos no sean iguales y que la relación no exista
+            if node_a != node_b and frozenset([node_a, node_b]) not in visited_edges_set:
+                break
+        else:
+            # Si el bucle for no se rompe (no encontramos nodos válidos), continuamos con la siguiente iteración
+            continue
+
+        # Crear el chat entre los dos usuarios
+        new_chat = Chat(node_a, node_b)
+        new_chat.add_lines(f"./chats/chat{i}.txt")
+
+        # Hacer que la arista que conecta a los dos usuarios/nodos sea el chat
+        graph_users.add_edge(node_a, node_b, new_chat)
+
+        # Agregar la relación a la lista de relaciones ya existentes
+        visited_edges_set.add(frozenset([node_a, node_b]))
+
+    graph_users.print_graph()
+
+    print("\n", "-"*50)
+    # Imprimir los usuarios/nodos con más amigos (Nodos con mayor grado)
+    print("Usuario(s) con más amigos:")
+    popular_users, top_3_users = graph_users.get_nodes_with_most_edges()
+    for user in popular_users:
+        print(f"   - {user}")
+    
+    # Imprimir el top 3 de usuarios/nodos con más amigos
+    print("\n", "-"*50)
+    print("Top 3 de usuarios con más amigos:")
+    for i in range(len(top_3_users)):
+        print(f"   {i+1}. {top_3_users[i]}")
+
+    # Imprimir la relación más fuerte entre dos usuarios/nodos
+    print("\n", "-"*50)
+    print("Relación más fuerte entre dos usuarios:")
+    stronger_relationships = graph_users.get_strongest_edge()
+    for relationship in stronger_relationships:
+        print(f"   - {relationship.weight}")
